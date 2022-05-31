bc0db25840d15e0f3f7ea6a197cd0f5b96d4ca5a
RMatthieu <105589955+RMatthieu@users.noreply.github.com>
2022-05-31 14:52:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,85 @@
 SCookie = document.getElementById("Cookie")
 SPlayground = document.getElementById("Playground")
+Samogus1 = document.getElementById("Amogus1")
+H2Text = document.getElementById("ScoreH2");
 
 coords = getOffset(SPlayground)
 
+
 Cookie = {x: 0, y: 0, Terrain: SCookie}
+Amogus1 = {x: 0, y: 0, Terrain: Samogus1}
 
 
-Cookie.set = function (x,y) {
-    box = document.getElementById("Cookie");
-    hauteur = box.offsetHeight;
-    largeur = box.offsetWidth;
-    if (x < 0) {
-        x=0;
-        
+class Sprite {
+    constructor(id) {
+        this.id = id;
+        this.dom = document.getElementById(id);
+        this.hauteur = this.dom.offsetHeight;
+        this.largeur = this.dom.offsetWidth;
     }
-    if (y < 0) {
-        y=0;
+    setPos(x,y) {
+        if (x < 0) {
+            x=0;
+            
+        }
+        if (y < 0) {
+            y=0;
+        }
+        if (x > SPlayground.offsetWidth - this.largeur) {
+            x = SPlayground.offsetWidth - this.largeur;
+            
+        }
+        if (y > SPlayground.clientHeight - this.hauteur) {
+            y=SPlayground.clientHeight - this.hauteur;
+        }            
+        this.dom.style.left = x + "px";
+        this.dom.style.top = y + "px";
+        this.x = x;
+        this.y = y;
     }
-    if (x > SPlayground.clientWidht + largeur) {
-        x = SPlayground.clientWidht + largeur;
-        
+    move(x,y) {
+        this.x += x;
+        this.y += y;
+        this.setPos(x,y);
     }
-    if (y > SPlayground.clientHeight - hauteur) {
-        y=SPlayground.clientHeight - hauteur;
-    }
-    
-    this.Terrain.style.left = x + "px";
-    this.Terrain.style.top = y + "px";
-    this.x = x;
-    this.y = y;
 }
 
+score = 0;
 
-Cookie.move = function (x,y) {
-    this.x += x;
-    this.y += y;
-    this.set(this.x,this.y);
+H2Text.setScore = function(score) {
+    this.textContent = "Score : " + score;
+}
+
+cookie = new Sprite("Cookie")
+
+amogus = [] 
+
+for (i=0; i < 4; i++) {
+    amogus[i] = new Sprite("Amogus" + (i + 1))
 }
 
 document.onclick = function(e ) {
-    i = 0;
 
-    //Taille Cookie
-    box = document.getElementById("Cookie");
-    hauteur = box.offsetHeight/2;
-    largeur = box.offsetWidth/2;
 
-    //Coords du cookie
-    x=Cookie.x; 
-    y=Cookie.y; 
-
+/* 
     //Coords souris
-    xs = e.clientX -coords.left;
-    ys = e.clientY - coords.top;
+    x = e.clientX - coords.left;
+    y = e.clientY - coords.top;
 
     //Calcule
     goX = (xs - x - largeur)/100;
     goY = (ys - y - hauteur)/100;
 
+    agoX = (xs - ax - largeur)/100;
+    agoY = (ys - ay - hauteur)/100;
 
     while (i < 100) {
         Cookie.move(goX,goY)
-        i++;
-    }
+        Amogus1.move(agoX,agoY)
+        i++
+    } */
+    score++;
+    H2Text.setScore(score);
 }
 
 n = 10;
